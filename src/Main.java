cd9a1c24f19b19895ee81dec08f9c981f099893e
Jennifer Talavage <jtalavage@gmail.com>
2026-09-22 09:38:37 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Jennifer Talavage");
+        System.out.println("Information Technology");
+        System.out.println("2026");
+    }
+}
